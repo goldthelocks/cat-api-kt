2a9d9c01209ae8dcbd616a0457d76e4f3453ad7e
goldthelocks <eraine.otayde@gmail.com>
2022-08-04 10:35:41 +0800

    Integrate to buildkite

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-# Builds and publishes image to docker repo
-
 IMAGE_NAME=goldthelocks/cat-api-kt
+BRANCH=${BUILDKITE_BRANCH:-snapshot}
 BUILD_COMMIT=$(git rev-parse --short HEAD)
 
-echo "==================================================="
-echo "Building app..."
-echo "==================================================="
-./mvnw clean install
+log_message() {
+  MESSAGE=$1
 
-echo "==================================================="
-echo "Building docker image..."
-echo "==================================================="
+  echo "==================================================="
+  echo $MESSAGE
+  echo "==================================================="
+}
 
-docker build \
-  --file Dockerfile \
+log_message "Building docker image..."
+
+DOCKER_TAGS="-t $IMAGE_NAME:$BRANCH"
+
+if [ "$BUILDKITE" == "true" ]; then
+  buildkite-agent artifact download "target/*" target/
+fi
+
+if [ "$BUILDKITE" == "true" ]; then
+  log_message "Doing buildkite docker build..."
+
+  if [ "$BUILDKITE_BRANCH" == "master" ]; then
+    DOCKER_TAGS="$DOCKER_TAGS -t $IMAGE_NAME:release_$BUILD_COMMIT"
+    DOCKER_TAGS="$DOCKER_TAGS -t $IMAGE_NAME:latest"
+  fi
+else
+  log_message "Doing local docker build..."
+
+  DOCKER_TAGS="$DOCKER_TAGS -t $IMAGE_NAME:latest"
+fi
+
+docker build . \
   --no-cache \
-  --tag ${IMAGE_NAME}:latest \
-  --tag ${IMAGE_NAME}:${BUILD_COMMIT} \
-  .
+  -f Dockerfile \
+  $DOCKER_TAGS
